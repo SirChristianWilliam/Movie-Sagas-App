79fff73b809e3f577d227cd6a8990d3a42d33a15
Chris Maki <c.william.developer@gmail.com>
2022-11-05 23:01:20 -0500

    Create spacing between movies on home page

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
 import { useHistory } from 'react-router-dom';
@@ -6,6 +6,24 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { Container } from '@mui/system';
 
+//MUI CARD
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+// import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import { IconButton } from '@mui/material';
+import { DeleteForeverOutlined } from '@mui/icons-material';
+// import CardActions from '@mui/material/CardActions';
+// import Collapse from '@mui/material/Collapse';
+// import Avatar from '@mui/material/Avatar';
+// import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+// import Typography from '@mui/material/Typography';
+// import { red } from '@mui/material/colors';
+// import FavoriteIcon from '@mui/icons-material/Favorite';
+// import ShareIcon from '@mui/icons-material/Share';
+// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+// import MoreVertIcon from '@mui/icons-material/MoreVert';
+//END MUI CARDS
 function MovieList() {
     const dispatch = useDispatch();
 
@@ -23,7 +41,7 @@ function MovieList() {
                     <h1>MovieList</h1>
                     <Grid container >
                         {/* xs={3} sm={6} md={12} */}
-                        <Grid  >
+                        <Grid spacing={8} >
                             <section className="movies">
 
                                 {movies.map(movie => {
@@ -31,16 +49,22 @@ function MovieList() {
                                     return (
                                         <div key={movie.id}>
                                             <h3>{movie.title}</h3>
-                                            <Paper >
+                                            <Card >
+                                                <CardHeader 
+                                                action={
+                                                    <IconButton>
+                                                    <DeleteForeverOutlined />
+                                                  </IconButton>
+                                                }/>
                                                 <img
                                                     src={movie.poster}
                                                     alt={movie.title}
                                                     onClick={() => {
                                                         history.push(`/${movie.id}`)
-                                                    }
+                                                        }
                                                     }
                                                 />
-                                            </Paper>
+                                            </Card>
                                         </div>
 
                                     );
