62f226075054527ec557918d64cf738dfdc006b4
Chris Maki <c.william.developer@gmail.com>
2022-11-06 13:07:07 -0600

    Change website header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,25 @@ import './App.css';
 import MovieList from '../MovieList/MovieList'
 import DetailsPage from '../DetailsPage/DetailsPage';
 import AddMoviePage from '../AddMoviePage/AddMoviePage';
-
+import CardHeader from '@mui/material/CardHeader';
 
 function App() {
   return (
     <div className="App">
-      <h1>The Movies Saga!</h1>
+      
+      <header id="header">
+      <h1 className='netclips'>
+        <span className="N">N</span>
+        <span className="e">e</span>
+        <span className="t">t</span>
+        <span className="c">c</span>
+        <span className="l">l</span>
+        <span className="i">i</span>
+        <span className="p">p</span>
+        <span className="s">s</span>
+        
+        </h1>
+      </header>
       
       <Router>        
         <Route path="/" exact>
